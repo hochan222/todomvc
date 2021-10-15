cbfbe88e549bae289c04ce0e6fc55b7832bf22a4
hochan222 <hochan049@gmail.com>
2021-10-15 15:03:01 +0900

    feat: 할 일을 등록할 수 있다.

--- src/component/TodoApp.tsx
+++ src/component/TodoApp.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import classNames from 'classnames';
 
+import StoreContext from '../contexts/stores';
 import TodoFooter from './TodoFooter';
 import TodoList from './TodoList';
 import TodoHeader from './TodoHeader';
 import TodoInput from './TodoInput';
+import { IRootStore } from '../types/models/index';
 
 function TodoApp(): React.ReactElement {
+  const rootStore: IRootStore = useContext(StoreContext) as IRootStore;
+
   return (
     <section>
       <TodoHeader />
-      <TodoInput />
+      <TodoInput store={rootStore} />
       <TodoList />
       <TodoFooter />
     </section>
--- src/component/TodoInput.tsx
+++ src/component/TodoInput.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { observer } from 'mobx-react-lite';
+import { IRootStore } from '../types/models';
 
-function TodoInput(): React.ReactElement {
-  return <input className="new-todo" placeholder="What needs to be done?" />;
+function TodoInput({ store: rootStore }: { store: IRootStore }): React.ReactElement {
+  const [content, setContent] = useState('');
+  const { addContent } = rootStore.todoStore;
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(e.target.value);
+  };
+
+  const onKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === 'Enter') {
+      addContent(content);
+    }
+  };
+
+  return (
+    <input
+      className="new-todo"
+      type="text"
+      value={content}
+      onChange={onChangeHandler}
+      onKeyDown={onKeyDownHandler}
+      placeholder="What needs to be done?"
+    />
+  );
 }
 
-export default TodoInput;
+export default observer(TodoInput);
